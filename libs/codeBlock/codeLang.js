08f92b20f890c31c4ed029644b0691ced5360240
Hanmur <1466046208@qq.com>
2022-02-24 11:21:27 +0800

    Site updated: 2022-02-24 11:21:27

--- libs/codeBlock/codeLang.js
+++ libs/codeBlock/codeLang.js
@@ -2,15 +2,13 @@
 
 $(function () {
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
-
   $('pre').before($highlight_lang);
   $('pre').each(function () {
     var code_language = $(this).attr('class');
-
     if (!code_language) {
       return true;
     };
-    var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
+    var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim().replace("lang-", "").trim();
 
     // 首字母大写
     // lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
